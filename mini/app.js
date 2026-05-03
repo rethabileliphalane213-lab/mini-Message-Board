89fb5d6c87378338eb1fd2b6878685e5580259c1
Rethabile <rethabileliphalane213@gmail.com>
2026-05-03 07:59:44 +0000

    final Comiit

--- mini/app.js
+++ mini/app.js
@@ -1,20 +1,36 @@
 const express=require("express")
 const app=express()
+const path=require("node:path")
 
+app.set("views",path.join(__dirname))
+app.set("view engine","ejs")
 
+app.use(express.urlencoded({ extended: true })) 
 const messages=[
 {
     text:"Hello Handsome",
     user:"crush",
-    add:new Date()
+    added:new Date()
 },
 {
-    text:"pheha papa",
+    text:"where are you?",
     user:"Thabelo",
-    add:new Date()
+    added:new Date()
 }
 ]
 
-app.get("/",(req,res)=>{
-    res.render("index",{title:"Mini Message Board",messages:messages})
+app.get("/new",(req,res)=>{
+    res.render("form",{title:"Mini Message Board",messages:messages})
+})
+
+app.post("/new",(req,res)=>{
+    const { user, text } = req.body;
+    messages.push({ user, text, added: new Date() });
+    res.redirect("/");
+})
+
+PORT=process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log(`Port running on ${PORT}`)
 })
